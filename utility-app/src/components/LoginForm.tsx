e758c5d97f5af99331d3a3ee9bc28a444cbd676a
Aceofthehood1 <geoffreyameble@gmail.com>
2023-12-20 19:43:13 +0000

    Addead

--- utility-app/src/components/LoginForm.tsx
+++ utility-app/src/components/LoginForm.tsx
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function LoginForm() {
-  let RegisterCode = '\n<form className="w-3/4 lg:w-2/4 flex flex-col bg-blue-500 rounded-lg justify-center items-center mt-5 mb-5">\n<h3 className="text-3xl m-2">Log In</h3>\n<input id="email" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5" type="email" placeholder="Email"/>\n<input id="password" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5" type="password" placeholder="Password"/>\n<button className="bg-red-500 w-28 h-10 rounded-lg m-3 hover:scale-110">Sign in</button>\n<a href="#" className="text-sm text-center lg:ml-44 mb-4 hover:text-red-600 items-end">Don\'t have an account? Click here to sign up</a>\n</form>' 
+  let RegisterCode =
+    '\n<form className="w-11/12 lg:w-2/4 flex flex-col bg-blue-500 rounded-lg justify-center items-center mt-5 mb-5">\n<h3 className="text-5xl m-2">Log In</h3>\n<div className="ml-2 flex flex-row justify-center items-center">\n<FontAwesomeIcon icon="envelope" className="h-16" />\n<input id="email" className="text-xl w-60 lg:w-80 h-16 rounded-lg m-5 p-5" type="email" placeholder="Email"/>\n</div>\n<div className="ml-2 flex flex-row justify-center items-center">\n<FontAwesomeIcon icon="lock" className="h-16" />\n<input id="password" className="text-xl w-60 lg:w-80 h-16 rounded-lg m-5 p-5" type="password" placeholder="Password"/>\n</div>\n<button className="w-28 h-16 m-3 p-2 rounded-lg relative overflow-hidden border border-red-900 bg-red-600 shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-orange-600 before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-orange-600 after:duration-500 hover:text-white hover:shadow-blue-900 hover:before:h-2/4 hover:after:h-2/4">\n<span className="relative text-lg z-10">Sign up</span>\n</button>\n<a href="#" className="text-lg text-center mb-5 lg:mb-4 transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full hover:text-red-600">Already have an account? Click here to sign in</a>\n</form>';
   async function handleCopyToClipboard() {
     try {
       await navigator.clipboard.writeText(RegisterCode!);
@@ -12,44 +15,81 @@ function LoginForm() {
     alert("You have copied to clipboard");
   }
 
-    return (
-      <>
-        <form className="w-3/4 lg:w-2/4 flex flex-col bg-blue-500 rounded-lg justify-center items-center mt-5 mb-5">
-            <h3 className="text-3xl m-2">Log In</h3>
-            <input id="email" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5" type="email" placeholder="Email"/>
-            <input id="password" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5" type="password" placeholder="Password"/>
-            <button className="bg-red-500 w-28 h-10 rounded-lg m-3 hover:scale-110">Sign in</button>
-            <a href="#" className="text-sm text-center lg:ml-44 mb-4 hover:text-red-600 items-end">Don't have an account? Click here to sign up</a>
-        </form>
+  return (
+    <>
+      <form className="w-11/12 lg:w-2/4 flex flex-col bg-blue-500 rounded-lg justify-center items-center mt-5 mb-5">
+        <h3 className="text-5xl m-2">Log In</h3>
+        <div className="ml-2 flex flex-row justify-center items-center">
+          <FontAwesomeIcon icon="envelope" className="h-16" />
+          <input
+            id="email"
+            className="text-xl w-60 lg:w-80 h-16 rounded-lg m-5 p-5"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="ml-2 flex flex-row justify-center items-center">
+          <FontAwesomeIcon icon="lock" className="h-16" />
+          <input
+            id="password"
+            className="text-xl w-60 lg:w-80 h-16 rounded-lg m-5 p-5"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <button className="w-28 h-16 m-3 p-2 rounded-lg relative overflow-hidden border border-red-900 bg-red-600 shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-orange-600 before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-orange-600 after:duration-500 hover:text-white hover:shadow-blue-900 hover:before:h-2/4 hover:after:h-2/4">
+          <span className="relative text-lg z-10">Sign up</span>
+        </button>
+        <a
+          href="#"
+          className="text-lg text-center mb-5 lg:mb-4 transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full hover:text-red-600"
+        >
+          Already have an account? Click here to sign in
+        </a>
+      </form>
 
-        <h3 className="text-2xl lg:text-3xl m-2">Code</h3>
-        <code className="flex flex-col justify-center items-center bg-orange-600 p-5 rounded-lg w-11/12 mb-10 text-xs">
-          <p id="clipboard">
-            &lt;form className="w-3/4 lg:w-2/4 flex flex-col bg-blue-500
-            rounded-lg justify-center items-center mt-5 mb-5"&gt;<br></br>
-            &lt;h3 className="text-3xl m-2"&gt;Log In&lt;/h3&gt;<br></br>
-            &lt;input id="email" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5"
-            type="text" placeholder="Email"/&gt;<br></br>
-            &lt;input id="password" className="text-lg w-60 lg:w-80 h-12 rounded-lg m-5"
-            type="text" placeholder="Password"/&gt;<br></br>
-            &lt;button className="bg-red-500 w-28 h-10 rounded-lg m-3
-            hover:scale-110"&gt;Sign in&lt;/button&gt;<br></br>
-            &lt;a href="#" className="text-sm lg:ml-44 mb-4 hover:text-red-600
-            items-end"&gt;Dont have an account? Click here to sign up&lt;/a&gt;<br></br>
-            &lt;/form&gt;
-          </p>
-          <button
-            className="p-2 bg-blue-600 w-40 h-10 rounded-lg hover:scale-110"
-            onClick={handleCopyToClipboard}
-          >
-            Copy to Clipboard
-          </button>
-        </code>
-      
-      
+      <h3 className="text-5xl m-2">Code</h3>
+      <code className="flex flex-col justify-center items-center bg-orange-600 p-5 rounded-lg w-11/12 mb-10 text-lg">
+        <p id="clipboard">
+          &lt;form className="w-11/12 lg:w-2/4 flex flex-col bg-blue-500
+          rounded-lg justify-center items-center mt-5 mb-5"&gt;<br></br>
+          &lt;h3 className="text-5xl m-2"&gt;Log In&lt;/h3&gt;<br></br>
+          &lt;div className="ml-2 flex flex-row justify-center items-center"&gt;
+          <br></br>
+          &lt;FontAwesomeIcon icon="envelope" className="h-16" /&gt; <br></br>
+          &lt;input id="email" className="text-xl w-60 lg:w-80 h-16 rounded-lg
+          m-5 p-5" type="email" placeholder="Email"/&gt;<br></br> &lt;/div&gt;
+          <br></br> &lt;div className="ml-2 flex flex-row justify-center
+          items-center"&gt;<br></br>
+          &lt;FontAwesomeIcon icon="lock" className="h-16" /&gt; <br></br>
+          &lt;input id="password" className="text-xl w-60 lg:w-80 h-16
+          rounded-lg m-5 p-5" type="password" placeholder="Password"/&gt;
+          <br></br> &lt;/div&gt;<br></br> &lt;button className="w-28 h-16 m-3
+          p-2 rounded-lg relative overflow-hidden border border-red-900
+          bg-red-600 shadow-2xl transition-all before:absolute before:left-0
+          before:right-0 before:top-0 before:h-0 before:w-full
+          before:bg-orange-600 before:duration-500 after:absolute after:bottom-0
+          after:left-0 after:right-0 after:h-0 after:w-full after:bg-orange-600
+          after:duration-500 hover:text-white hover:shadow-blue-900
+          hover:before:h-2/4 hover:after:h-2/4"&gt;<br></br>
+          &lt;span className="relative text-lg z-10"&gt;Sign up&lt;/span&gt;
+          <br></br>
+          &lt;/button&gt; <br></br>&lt;a href="#" className="text-lg text-center
+          mb-5 lg:mb-4 transition-all before:absolute before:left-0
+          before:right-0 before:top-0 before:h-0 before:w-full
+          hover:text-red-600"&gt;Already have an account? Click here to sign
+          in&lt;/a&gt;<br></br>
+          &lt;/form&gt;
+        </p>
+        <button
+          className="p-2 mt-5 w-60 h-16 rounded-lg relative overflow-hidden border border-blue-900 bg-blue-600 shadow-2xl transition-all before:absolute before:left-0 before:right-0 before:top-0 before:h-0 before:w-full before:bg-red-600 before:duration-500 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0 after:w-full after:bg-red-600 after:duration-500 hover:text-white hover:shadow-green-900 hover:before:h-2/4 hover:after:h-2/4"
+          onClick={handleCopyToClipboard}
+        >
+          <span className="relative z-10">Copy to Clipboard</span>
+        </button>
+      </code>
+    </>
+  );
+}
 
-      </>
-    );
-  }
-  
-  export default LoginForm;
+export default LoginForm;
--- utility-app/src/components/Tailwind.tsx
+++ utility-app/src/components/Tailwind.tsx
@@ -3,16 +3,16 @@ import RegistrationForm from "./RegistrationForm";
 function Tailwind() {
     return (
       <>
-        <h1 className="text-4xl lg:text-5xl font-bold text-red-500 text-center">
+        <h1 className="text-4xl lg:text-7xl font-bold text-red-500 text-center">
           Tailwind Components
         </h1>
-        <h2 className="text-4xl text-center mt-20">Registration Form</h2>
-        <div className="bg-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5">
+        <h2 className="text-6xl text-center mt-20">Registration Form</h2>
+        <div className="bg-gradient-to-tl from-blue-400 via-cyan-900 to-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5">
           <RegistrationForm></RegistrationForm>
         </div>
         
-        <h2 className="text-4xl text-center mt-20">Log In Form</h2>
-        <div className="bg-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5 mb-10">
+        <h2 className="text-6xl text-center mt-20">Log In Form</h2>
+        <div className="bg-gradient-to-tl from-blue-400 via-cyan-900 to-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5 mb-10">
           <LoginForm></LoginForm>
         </div>
       </>
