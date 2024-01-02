91965bc48acba837e9192d91705f28b184cb2a00
Geoffrey Ameble <geoffreyameble81@gmail.com>
2024-01-02 15:09:21 +0000

    Added hamburger menu to navigation bar with hyperlinks and added carousel component to project

--- utility-app/src/components/Tailwind.tsx
+++ utility-app/src/components/Tailwind.tsx
@@ -1,3 +1,4 @@
+import Carousel from "./Carousel";
 import LoginForm from "./LoginForm";
 import RegistrationForm from "./RegistrationForm";
 function Tailwind() {
@@ -6,14 +7,19 @@ function Tailwind() {
         <h1 className="text-4xl lg:text-7xl font-bold text-red-500 text-center">
           Tailwind Components
         </h1>
-        <h2 className="text-6xl text-center mt-20">Registration Form</h2>
+        <h2 className="text-6xl text-center mt-20" id="register">Registration Form</h2>
         <div className="bg-gradient-to-tl from-blue-400 via-cyan-900 to-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5">
           <RegistrationForm></RegistrationForm>
         </div>
         
-        <h2 className="text-6xl text-center mt-20">Log In Form</h2>
+        <h2 className="text-6xl text-center mt-20" id="login">Log In Form</h2>
         <div className="bg-gradient-to-tl from-blue-400 via-cyan-900 to-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5 mb-10">
           <LoginForm></LoginForm>
+        </div>
+
+        <h2 className="text-6xl text-center mt-20" id="carousel">Carousel</h2>
+        <div className="bg-gradient-to-tl from-blue-400 via-cyan-900 to-green-500 w-11/12 flex flex-col justify-center items-center rounded-lg mt-5 mb-10">
+          <Carousel></Carousel>
         </div>
       </>
     );
